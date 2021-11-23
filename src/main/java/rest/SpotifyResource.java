67ae7e2d462b0c861dcd0b485b1adc15fea9bf5b
MagnusGranno <70953393+MagnusGranno@users.noreply.github.com>
2021-11-23 13:57:02 +0100

    adds test for browse endpoint

--- src/main/java/rest/SpotifyResource.java
+++ src/main/java/rest/SpotifyResource.java
@@ -11,6 +11,7 @@ import javax.persistence.EntityManagerFactory;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import java.io.IOException;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
@@ -32,7 +33,7 @@ public class SpotifyResource {
     }
 
     @GET
-    @Produces
+    @Produces({MediaType.APPLICATION_JSON})
     @Path("browse")
     public String getCategoriesFromSpotify() throws IOException {
 
